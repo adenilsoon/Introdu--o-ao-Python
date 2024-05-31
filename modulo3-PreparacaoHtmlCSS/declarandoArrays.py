9c9daba5bd76f62f58998f0730af391f82f2f2d8
Adenilson <sradenilson.sr16@gmail.com>
2024-05-31 14:56:49 -0300

    Criação do arquivo declarandoArrays.py

---
+++ modulo3-PreparacaoHtmlCSS/declarandoArrays.py
@@ -0,0 +1,10 @@
+produtosLoja = ["Capinha Iphone 13", "Carregador turbo", "Película 3D", "Fone de ouvido", "Ring Light"];
+anosNascimento = [1973, 1975, 1995, 2000, 2002, 2017];
+
+print("Listando produtos da loja:");
+print(produtosLoja);
+
+print("\n");
+
+print("Listando anos de nascimento: ");
+print(anosNascimento);
