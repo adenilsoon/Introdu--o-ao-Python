b217d1712c7b3165c401e0648d53618f4357fbdb
Adenilson <sradenilson.sr16@gmail.com>
2024-05-31 15:26:00 -0300

    Criação do arquivo manipulandoArray.py

---
+++ modulo3-PreparacaoHtmlCSS/manipulandoArray.py
@@ -0,0 +1,12 @@
+#TODO imprima no terminal: 
+# A quantidade de elementos que ele possui
+# O dado salvo no índice 2
+# O dado salvo no índice 9
+# O dado salvo no índice 14.
+
+lista_musicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa'];
+
+print(len(lista_musicos));
+print(lista_musicos[2]);
+print(lista_musicos[9]);
+print(lista_musicos[14]);
