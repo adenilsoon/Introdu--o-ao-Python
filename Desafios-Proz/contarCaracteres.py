3b5996369a8e3192e885f5448f58863085ac79ca
Adenilson <sradenilson.sr16@gmail.com>
2024-06-10 20:46:01 -0300

    commit

--- Desafios-Proz/contarCaracteres.py
+++ Desafios-Proz/contarCaracteres.py
@@ -1,19 +1,21 @@
 def contarCaracteres():
+    
     executando = True;
 
     while(executando):
         
-        texto = input("Informe um texto ou palavra: ");
-        opcao = int(input("Informe o número corresponde a informação que você forneceu acima: \n\n1 - Palavra\n2 - Texto\n3 - Sair\n\n>>> "));
+        opcao = int(input("O que você deseja fazer:\n\n1 - Contar caracteres de uma palavra\n2 - Contar palavras de um texto\n3 - Sair do programa\n\n>>> "));
+        print("\n")
 
         if(opcao == 1):
+            texto = input("Informe uma palavra: ");
             totalCaracteres = len(texto);
             print("A palavra fornecida possui " + str(totalCaracteres) + " caracteres");
         
         elif(opcao == 2):
+            texto = input("Informe um texto: ");
             palavras = texto.split();
-            totalPalavras = len(palavras);
-            print("O texto informado possui " + str(totalPalavras) + " palavras");
+            print("O texto informado possui " + str(len(palavras)) + " palavras");
         elif(opcao == 3):
             executando = False
             print("Fechando programa...");
