fdae65110bd634567aecf7d3cae46efb7b7da240
Adenilson <sradenilson.sr16@gmail.com>
2024-06-10 21:10:19 -0300

    changes

--- Desafios-Proz/contarCaracteres.py
+++ Desafios-Proz/contarCaracteres.py
@@ -1,25 +1,26 @@
 def contarCaracteres():
     
     executando = True;
+    
+    try:
+        while(executando):
+            
+            opcao = int(input("\nO que você deseja fazer:\n\n1 - Contar caracteres de uma palavra\n2 - Contar palavras de um texto\n3 - Sair do programa\n\n>>> "));
 
-    while(executando):
-        
-        opcao = int(input("O que você deseja fazer:\n\n1 - Contar caracteres de uma palavra\n2 - Contar palavras de um texto\n3 - Sair do programa\n\n>>> "));
-        print("\n")
-
-        if(opcao == 1):
-            texto = input("Informe uma palavra: ");
-            totalCaracteres = len(texto);
-            print("A palavra fornecida possui " + str(totalCaracteres) + " caracteres");
-        
-        elif(opcao == 2):
-            texto = input("Informe um texto: ");
-            palavras = texto.split();
-            print("O texto informado possui " + str(len(palavras)) + " palavras");
-        elif(opcao == 3):
-            executando = False
-            print("Fechando programa...");
-        else:
-            print("\n\nDigite uma opção válida");
-        
+            if(opcao == 1):
+                texto = input("Informe uma palavra: ");
+                totalCaracteres = len(texto.replace(" ",""));
+                print("A palavra fornecida possui " + str(totalCaracteres) + " caracteres");
+            
+            elif(opcao == 2):
+                texto = input("Informe um texto: ");
+                palavras = texto.split();
+                print("O texto informado possui " + str(len(palavras)) + " palavras");
+            elif(opcao == 3):
+                executando = False
+                print("Fechando programa...");
+            else:
+                print("\nDigite uma opção válida");
+    except: 
+        print("\nInformação inválida, tente novamente\n");   
 contarCaracteres();
