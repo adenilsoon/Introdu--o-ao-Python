b34d98b2cc11f27c7a0f3bc283a16f5d66a1014f
Adenilson <sradenilson.sr16@gmail.com>
2024-07-14 17:19:50 -0300

    Criação site do ponto turístico Marco Zero de Recife/PE

---
+++ modulo5-JavaScript1/aula01/aula01.js
@@ -0,0 +1,11 @@
+const id = 111
+let userName = "Adenilson"
+
+console.log(id, userName)
+
+if(id == 111 || userName == "Adenilson"){
+  console.log("Deseja mudar sua senha?")
+}
+else {
+  console.log("Você não pode mudar sua senha.")
+}
